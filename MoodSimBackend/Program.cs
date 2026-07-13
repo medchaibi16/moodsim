edf9397b2ff6d97dc606fc2e3eb7025e4acabcfa
medchaibi16 <medchaibi965@proton.me>
2026-07-13 15:33:20 +0100

    Add Docker support for MoodStabilizer — one-command setup

--- MoodSimBackend/Program.cs
+++ MoodSimBackend/Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
